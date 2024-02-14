d48397624213ce4466ce4c9b3cd2bdb667536cca
Mamadou <mamadou.thiongane@grenoble-inp.org>
2024-02-14 16:57:28 +0100

    Mise à jour de l’état d’une cellule

--- public_html/js/myScript.js
+++ public_html/js/myScript.js
@@ -1,23 +1,69 @@
-const cellSize = 20 
+const cellSize = 20
 const size = 30
 
 const life = {
-    // A compléter
+    board: new Array(),
+    borderX: function () { return (paper.view.size.width - size * cellSize) / 2 },
+    borderY: function () { return (paper.view.size.height - size * cellSize) / 2 },
+
+    coords: function (i, j) {
+        x = this.borderX() + i * cellSize
+        y = this.borderY() + j * cellSize
+        return [x, y]
+    },
+
+    init: function() {
+        for (let i = 0; i < size; i++) {
+            this.board[i] = new Array()
+
+            for (let j = 0; j < size; j++) {
+                this.board[i][j] = new Cell(i, j)
+            }
+        }
+    },
 }
 
 class Cell {
-// A compléter
+    #i
+    #j
+    #state = 0
+    #previousState = 0
+
+    #shape = paper.Path.Circle({
+        center: life.coords(this.#i, this.#j),
+        radius: cellSize / 2,
+        fillColor: 'white',
+        strokeColor: 'blue'
+    })
+
+    constructor(i, j) {
+        this.#i = i
+        this.#j = j
+    }
+
+    live() {
+        this.#state = 1
+        this.#shape.fillColor = 'cyan'
+    }
+
+    die() {
+        this.#state = 0
+        this.#shape.fillColor = 'white'
+    }
 }
-function onKeyUp(event){
+
+function onKeyUp(event) {
     // A Compléter
 }
-window.addEventListener("keyup",onKeyUp);
+
+window.addEventListener("keyup", onKeyUp);
 
 window.addEventListener("load",
-    function(){
+    function () {
         let canvas = document.getElementById("myCanvas")
         paper.setup(canvas)
         // Placer le code à exécuter ici pour qu'il le soit une fois la page 
         // chargée dans son intégralité
+        life.init()
     }
 )
