13c992cf28f883efad868f4efcc66135f88a9d0d
Mamadou <mamadou.thiongane@grenoble-inp.org>
2024-02-15 17:24:45 +0100

    Badges

--- genereBadges.sh
+++ genereBadges.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-NBERR=$(grep -e "  error" js_report.txt | wc -l)
-NBWARN=$(grep -e "  warning" js_report.txt | wc -l)
-color="green"
+for f in Exercice-31 Exercice-32 Exercice-33 Conway; do
+    NBERR=$(grep -e "  error" "$f"_report.txt | wc -l)
+    NBWARN=$(grep -e "  warning" "$f"_report.txt | wc -l)
+    color="green"
 
-if [[ $NBERR > 0 ]]; then
-    color="red"
-else
-    if [[ $NBWARN > 0 ]]; then
-        color="orange"
+    if [[ $NBERR > 0 ]]; then
+        color="red"
+    else
+        if [[ $NBWARN > 0 ]]; then
+            color="orange"
+        fi
     fi
-fi
 
-anybadge -o -l "lint-js" -v "$NBERR $NBWARN" -c "$color" -f "badge_lint.svg"
+    anybadge -o -l "$f" -v "$NBERR $NBWARN" -c "$color" -f "$f"_lint.svg
+done
