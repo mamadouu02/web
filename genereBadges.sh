60bb504e1599bef7571a318f8e9cd505c9330638
Mamadou <mamadou.thiongane@grenoble-inp.org>
2024-02-15 16:21:33 +0100

    Badges

---
+++ genereBadges.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NBERR=$(grep -e "^error" js_report.txt | wc -l)
+NBWARN=$(grep -e "^warning" js_report.txt | wc -l)
+color="green"
+
+if [[ $NBERR > 0 ]]; then
+    color="red"
+else
+    if [[ $NBWARN > 0 ]]; then
+        color="orange"
+    fi
+fi
+
+anybadge -o -l "LINT" -v "$NBERR $NBWARN" -c "$color" -f "badge_lint.svg"
