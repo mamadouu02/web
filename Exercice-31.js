ac3d2fb0741388c30aa3e487f1b27784260ce279
Mamadou <mamadou.thiongane@grenoble-inp.org>
2024-02-22 01:01:50 +0100

    CI/CD

--- Exercice-31.js
+++ Exercice-31.js
@@ -1,21 +1,21 @@
-function miseEnPlace(){
-    const corps=document.querySelector('body')
-    const paragraphe=document.createElement("ul")
-    const tilde_1 = document.createElement("li")
-    const texte_1=document.createTextNode("one")
-    const tilde_2 = document.createElement("li")
-    const texte_2=document.createTextNode("two")
-    const tilde_3 = document.createElement("li")
-    const texte_3=document.createTextNode("three")
-    tilde_1.appendChild(texte_1)
-    tilde_2.appendChild(texte_2)
-    tilde_3.appendChild(texte_3)
-    paragraphe.appendChild(tilde_1)
-    paragraphe.appendChild(tilde_2)
-    paragraphe.appendChild(tilde_3)
+function miseEnPlace() {
+    const corps = document.querySelector('body')
+    const paragraphe = document.createElement("ul")
+    const tilde1 = document.createElement("li")
+    const texte1 = document.createTextNode("one")
+    const tilde2 = document.createElement("li")
+    const texte2 = document.createTextNode("two")
+    const tilde3 = document.createElement("li")
+    const texte3 = document.createTextNode("three")
+    tilde1.appendChild(texte1)
+    tilde2.appendChild(texte2)
+    tilde3.appendChild(texte3)
+    paragraphe.appendChild(tilde1)
+    paragraphe.appendChild(tilde2)
+    paragraphe.appendChild(tilde3)
     corps.appendChild(paragraphe)
-    tilde_3.className="last"
+    tilde3.className = "last"
+}
 
-  }
-  // La fonction miseEnPlace ne sera appelée qu'une fois la page entièrement chargée
-  window.addEventListener("load",miseEnPlace,false)
+// La fonction miseEnPlace ne sera appelée qu'une fois la page entièrement chargée
+window.addEventListener("load", miseEnPlace, false)
--- Exercice-32.js
+++ Exercice-32.js
@@ -1,40 +1,38 @@
+/* eslint-disable no-unused-vars */
 
-function miseEnPlace(){
-    const corps=document.querySelector('body')
+function miseEnPlace() {
+    const corps = document.querySelector('body')
+    document.getElementById("b1").addEventListener("click", yellow, false)
+    document.getElementById("b2").addEventListener("click", change, false)
+    document.getElementById("b3").addEventListener("click", transform, false)
+}
 
-    document.getElementById("b1").addEventListener("click",yellow,false)
-    document.getElementById("b2").addEventListener("click",change,false)
-    document.getElementById("b3").addEventListener("click",transform,false)
+// La fonction miseEnPlace ne sera appelée qu'une fois la page entièrement chargée
+window.addEventListener("load", miseEnPlace, false)
 
-  }
-  // La fonction miseEnPlace ne sera appelée qu'une fois la page entièrement chargée
-  window.addEventListener("load",miseEnPlace,false)
-
-  function yellow(event){
+function yellow(event) {
     // this est l'élément auquel on aura attaché l'évènement
-    const contents = document.getElementById("contents"); 
-    contents.style.backgroundColor= 'yellow'
-  }
+    const contents = document.getElementById("contents");
+    contents.style.backgroundColor = 'yellow'
+}
 
-  function change(event){
+function change(event) {
     const title = document.querySelector("h1")
-    if (title.style.color === 'red'){
-      title.style.color = "#66f"
-    }else{
-      title.style.color ='red'
+
+    if (title.style.color === 'red') {
+        title.style.color = "#66f"
+    } else {
+        title.style.color = 'red'
     }
+}
 
-  }
-
-  function transform(event){
+function transform(event) {
     let paragraph = document.querySelectorAll("p")
-    for(var p of paragraph.values()){
-      if (p.style.fontStyle !== 'italic'){
-        p.style.fontStyle = 'italic'
-        return
-      }
-    }
-
     
-  }
-
+    for (var p of paragraph.values()) {
+        if (p.style.fontStyle !== 'italic') {
+            p.style.fontStyle = 'italic'
+            return
+        }
+    }
+}
